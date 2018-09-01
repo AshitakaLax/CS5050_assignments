94afde8427c035c0eff19635d11d9ce072327f79
levi Balling <leviballing@gmail.com>
2018-09-01 14:57:29 -0600

    Finished adding support for both Knapsacks

--- Assignment2_knapsack/Knapsack.py
+++ Assignment2_knapsack/Knapsack.py
@@ -13,8 +13,6 @@ class Item:
 	"""
 	Size = 0
 
-	def Initialize(self, size):
-		self.Size = 0
 
 # This will represent a Knapsack
 # with the Number of Max size it can fill
@@ -69,7 +67,7 @@ def ConstantProblemGenerator(N, C):
 		problemSet.append(item)
 	return problemSet
 
-def KnapRecursive(n, l1, l2):
+def SingleKnapRecursive(n, l1, l2):
 	"""Recursive algorithm that returns true if both 
 	knap sacks are filled
 	
@@ -88,37 +86,76 @@ def KnapRecursive(n, l1, l2):
 	item = n.pop(0)
 	# this is if we put it in the knapsack or  This is if we want to discard the package
 	# to put it in the bag
-	return KnapRecursive(n, knapSackOne.Capacity - item.Size, 0) or KnapRecursive(n, knapSackOne.Capacity, 0)
+	return SingleKnapRecursive(n, knapSackOne.Capacity - item.Size, 0) or SingleKnapRecursive(n, knapSackOne.Capacity, 0)
+
+def KnapRecursive(n, l1, l2):
+	"""Recursive algorithm that returns true if both 
+	knap sacks are filled
+	
+	Args:
+		n (List<Item>): The list of Items to add to knapsacks
+		l1 (int): The Size of the first knapsack
+		l2 (int): The Size of the Second knapsack
+	"""
+	knapSackOne = Knapsack()
+	knapSackOne.Capacity = l1
+	knapSackTwo = Knapsack()
+	knapSackTwo.Capacity = l2
+	if(knapSackOne.Capacity < 0 or knapSackTwo.Capacity < 0):
+		return False
+	if(knapSackOne.Capacity == 0 and knapSackTwo.Capacity == 0):
+		return True
+	if(len(n) == 0):
+		return False
+	item = n.pop(0)
+	# this is if we put it in the knapsack or  This is if we want to discard the package
+	# to put it in the bag
+
+	return (KnapRecursive(n, knapSackOne.Capacity - item.Size, knapSackTwo.Capacity) # put in KnapsackOne
+	or KnapRecursive(n, knapSackOne.Capacity, knapSackTwo.Capacity - item.Size) # put in knapsackOne
+	or KnapRecursive(n, knapSackOne.Capacity, knapSackTwo.Capacity)) # discard the item
 # First solve this problem for One knapsack
 
-def runTest(n, k, expect):
-	print("Size of knapsack: "+ str(k))
+def runTest(n, k1, k2, expect):
+	print("Size of knapsackOne: "+ str(k1))
+	print("Size of knapsackTwo: "+ str(k2))
 	print("Number of objects: "+ str(len(n)))
 	print([item.Size for item in n])
-	result = KnapRecursive(n, k, 0)
+	result = KnapRecursive(n, k1, k2)
 	print("Can Fill all cells:" + str(result))
 	if(result == expect):
 		print("SUCCESS")
 	else:
 		print("FAILED")
-	print("Can Fill all cells:" + str(result))
-	return result
+	return result == expect
 
 # Create Test size sets
 # test data sets
-n = ProblemGenerator(0, 1)
+# Testing a Single Knapsack
+n = ConstantProblemGenerator(0, 1)
 print([item.Size for item in n])
-sumResult = runTest(n, 0, True)
-n = ProblemGenerator(1, 2)
-sumResult &= runTest(n, 0, True)
-n = ProblemGenerator(50, 3)
-sumResult &= runTest(n, 16, True)
-n = ProblemGenerator(5, 10)
-sumResult &= runTest(n, 100, True)
+sumResult = runTest(n, 0, 0, True)
+n = ConstantProblemGenerator(1, 2)
+sumResult &= runTest(n, 0, 0, True)
+n = ConstantProblemGenerator(10, 2)
+sumResult &= runTest(n, 16, 0, True)
+n = ConstantProblemGenerator(20, 10)
+sumResult &= runTest(n, 100, 0, True)
 n = ConstantProblemGenerator(5, 1)
-sumResult &= runTest(n, 5, True)
-# sumResult &= runTest(4, 5, False)
-# sumResult &= runTest(4, 4, True)
+sumResult &= runTest(n, 5, 0, True)
+
+# Testing multiple Knapsacks
+n = ConstantProblemGenerator(5, 5)
+sumResult &= runTest(n, 20, 5, True)
+n = ConstantProblemGenerator(5, 5)
+sumResult &= runTest(n, 20, 6, False)
+n = ConstantProblemGenerator(3, 3)
+sumResult &= runTest(n, 3, 6, True)
+n = ConstantProblemGenerator(3, 3)
+sumResult &= runTest(n, 0, 6, True)
+n = ConstantProblemGenerator(3, 3)
+sumResult &= runTest(n, 0, 10, False)
+
 
 if(sumResult):
 	print("ALL Tests PASS")
